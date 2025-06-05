edd7b3d34e7df8aaaa53788771b72345c5b784af
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-05 13:55:29 +0530

    completed Member.cs class with Enums, Getter, Setter methods and Constructor

--- Library System/Member.cs
+++ Library System/Member.cs
@@ -8,5 +8,27 @@ namespace Library_System
 {
     internal class Member
     {
+        public enum MemberType
+        {
+            Member,
+            Staff,
+            Management
+        }
+
+        public required string Name { get; set; }
+        public int MemberID { get; set; }
+        public MemberType Type { get; set; }
+
+        public int BorrowedBooksCount { get; private set; } = 0;
+
+        public Member(string name, int memberId, MemberType type)
+        {
+            Name = name;
+            MemberID = memberId;
+            Type = type;
+        }
+
+
+
     }
 }
