8341ee7d1dd05012d3b09c85b2e91e6f4aa9ecee
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-05 12:13:25 +0530

    completed Book.cs class with fields, constructos and getter setter methods

---
+++ Library System/Book.cs
@@ -0,0 +1,62 @@
+﻿namespace LibrarySystem
+{
+    internal class Book
+    {
+        private String title;
+        private String author;
+        private int publicationYear;
+        private String catagory;
+        private bool isAvailable;
+
+        public Book(String title, String author, int publicationYear, String catagory)
+        {
+            this.title = title;
+            this.author = author;
+            this.publicationYear = publicationYear;
+            this.catagory = catagory;
+            this.isAvailable = true; // Default to available
+        }
+
+        public String Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public String Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public int PublicationYear
+        {
+            get { return publicationYear; }
+            set
+            {
+
+                if (value > 1450 && value > DateTime.Now.Year)
+                {
+                    publicationYear = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid year of publication");
+                }
+            }
+        }
+
+        public String Catagory
+        {
+            get { return catagory; }
+            set { catagory = value; }
+        }
+
+        public bool IsAvailable
+        {
+            get { return isAvailable; }
+            set { isAvailable = value; }
+        }
+    }
+}
