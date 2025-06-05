805aa8ad2bee07f1e246b5d74242ff90ef1072ca
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-05 12:17:08 +0530

    refactored the Book.cs class to simplify code readability

--- Library System/Book.cs
+++ Library System/Book.cs
@@ -2,61 +2,41 @@
 {
     internal class Book
     {
-        private String title;
-        private String author;
+        public string Title { get; set; }
+        public string Author { get; set; }
+
         private int publicationYear;
-        private String catagory;
-        private bool isAvailable;
-
-        public Book(String title, String author, int publicationYear, String catagory)
-        {
-            this.title = title;
-            this.author = author;
-            this.publicationYear = publicationYear;
-            this.catagory = catagory;
-            this.isAvailable = true; // Default to available
-        }
-
-        public String Title
-        {
-            get { return title; }
-            set { title = value; }
-        }
-
-        public String Author
-        {
-            get { return author; }
-            set { author = value; }
-        }
-
         public int PublicationYear
         {
             get { return publicationYear; }
             set
             {
 
-                if (value > 1450 && value > DateTime.Now.Year)
+                if (value >= 1450 && value <= DateTime.Now.Year)
                 {
                     publicationYear = value;
 
                 }
                 else
                 {
-                    Console.WriteLine("Enter a valid year of publication");
+                    throw new ArgumentOutOfRangeException(nameof(value), "Publication year must be between 1450 and the current year.");
                 }
             }
         }
+        private string Catagory { get; set; }
+        private bool IsAvailable { get; set; }
 
-        public String Catagory
+        public Book(string title, string author, int publicationYear, string catagory)
         {
-            get { return catagory; }
-            set { catagory = value; }
+            Title = title;
+            Author = author;
+            this.publicationYear = publicationYear;
+            Catagory = catagory;
+            IsAvailable = true; // Default to available
         }
 
-        public bool IsAvailable
-        {
-            get { return isAvailable; }
-            set { isAvailable = value; }
-        }
+        
     }
+
+
 }
