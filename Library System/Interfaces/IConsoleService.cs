6bdcd351577c9aa562b493862020afe95135f9dc
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-09 10:29:17 +0530

    segmenting main method into chunks of classes that will adhere SOLID principles. First created Authentication class and authentication logic

---
+++ Library System/Interfaces/IConsoleService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System.Interfaces
+{
+    public interface IConsoleService
+    {
+        void WriteLine(string message);
+        void Write(string message);
+        string ReadLine();
+        void Clear();
+    }
+}
