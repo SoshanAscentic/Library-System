204fade7c24092483f110231b7dc066c0e58b679
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-09 09:55:54 +0530

    added one main library interface to combine all services and refactored the main class to use the newly created interfaces and services

--- Library System/Interfaces/ILibrary.cs
+++ Library System/Interfaces/ILibrary.cs
@@ -11,13 +11,8 @@ namespace Library_System.Interfaces
 
     public interface ILibrary
     {
-        void AddBook(Book book);
-        Member AddMember(string name, int memberType);
-        void RemoveBook(string title, int publicationYear);
-        void BorrowBook(string title, int publicationYear, int memberId);
-        void ReturnBook(string title, int publicationYear, int memberId);
-        void DisplayBooks();
-        void DisplayMembers();
-        Member? GetMemberById(int memberId); 
+        IBookService BookService { get; }
+        IMemberService MemberService { get; }
+        IBorrowingService BorrowingService { get; } 
     }
 }
