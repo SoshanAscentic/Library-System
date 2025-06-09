fa21bf5d4d1b56dfdb9e9d4b130d76bb6627f824
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-09 10:40:26 +0530

    refactored and created a seperate class and interface for menu displaying

---
+++ Library System/Interfaces/IMenuService.cs
@@ -0,0 +1,16 @@
+﻿using Library_System.Models.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System.Interfaces
+{
+    public interface IMenuService
+    {
+        string? DisplayMainMenu();
+        string? DisplayAuthMenu();
+        void DisplayWelcome(Member member);
+    }
+}
---
+++ Library System/Services/MenuService.cs
@@ -0,0 +1,48 @@
+﻿using Library_System.Interfaces;
+using Library_System.Models.Members;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System.Services
+{
+    public class MenuService : IMenuService
+    {
+        private readonly IConsoleService consoleService;
+
+        public MenuService(IConsoleService consoleService)
+        {
+            this.consoleService = consoleService ?? throw new ArgumentNullException(nameof(consoleService));
+        }
+
+        public string? DisplayAuthMenu()
+        {
+            consoleService.WriteLine("Welcome to the Library System!");
+            consoleService.WriteLine("1. Sign up as a new member");
+            consoleService.WriteLine("2. Login with Member ID");
+            consoleService.Write("Please choose an option (1 or 2): ");
+            return consoleService.ReadLine();
+        }
+        public string? DisplayMainMenu()
+        {
+            consoleService.WriteLine("\n--- Library Dashboard ---");
+            consoleService.WriteLine("1. Add Book");
+            consoleService.WriteLine("2. Remove Book");
+            consoleService.WriteLine("3. Borrow Book");
+            consoleService.WriteLine("4. Return Book");
+            consoleService.WriteLine("5. Display Books");
+            consoleService.WriteLine("6. Display Members");
+            consoleService.WriteLine("0. Exit");
+            consoleService.Write("Choose an option: ");
+            return consoleService.ReadLine();
+        }
+
+        public void DisplayWelcome(Member member)
+        {
+            consoleService.Clear();
+            consoleService.WriteLine($"Welcome, {member.Name}! Type: {member.GetMemberType()}");
+        }
+    }
+}
