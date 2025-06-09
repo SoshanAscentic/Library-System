1b457006f14d101870c5aa3965b40ddf72984266
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-09 12:04:33 +0530

    created a factory classes that will relay the commands to their respective objects

--- Library System/Commands/AddBookCommand.cs
+++ Library System/Commands/AddBookCommand.cs
@@ -54,4 +54,3 @@ namespace Library_System.Commands
         }
     }
 }
-}
---
+++ Library System/Factories/CommandFactory.cs
@@ -0,0 +1,36 @@
+﻿using Library_System.Commands;
+using Library_System.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System.Factories
+{
+    public class CommandFactory : ICommandFactory
+    {
+        private readonly ILibrary library;
+        private readonly IConsoleService consoleService;
+
+        public CommandFactory(ILibrary library, IConsoleService consoleService)
+        {
+            this.library = library ?? throw new ArgumentNullException(nameof(library));
+            this.consoleService = consoleService ?? throw new ArgumentNullException(nameof(consoleService));
+        }
+
+        public Dictionary<string, ICommand> CreateCommands()
+        {
+            return new Dictionary<string, ICommand>
+            {
+                ["1"] = new AddBookCommand(library.BookService, consoleService),
+                ["2"] = new RemoveBookCommand(library.BookService, consoleService),
+                ["3"] = new BorrowBookCommand(library.BorrowingService, consoleService),
+                ["4"] = new ReturnBookCommand(library.BorrowingService, consoleService),
+                ["5"] = new DisplayBooksCommand(library.BookService, consoleService),
+                ["6"] = new DisplayMembersCommand(library.MemberService, consoleService)
+            };
+        }
+    }
+
+}
